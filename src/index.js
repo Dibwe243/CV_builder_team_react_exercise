266cfb713afb8775434df9c61f0b9443de616b46
TebogoMothelo <tebogo.mothelo@umuzi.org>
2019-10-11 11:28:12 +0200

    moved code to PlayListForm and changed Fragment to div

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import AppForm from './components/AppForm';
+import PlayListForm from './components/PlayListForm';
 
-ReactDOM.render(<AppForm />, document.getElementById('root'));
+ReactDOM.render(<PlayListForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
