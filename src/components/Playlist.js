a826b690692723d5a333a6414ba90ac4a99c4a21
PearlSithebe <pearlsithebe@gmail.com>
2019-10-11 11:51:48 +0200

    PlayerView awaiting other components

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,1 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+
+class PlayerView extends React.Component {
+    render() { 
+        return (
+            <div>
+        <h1>Music Player</h1>
+        </div>
+        );
+    }
+}
+ 
+export default PlayerView;
