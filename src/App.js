bd0daca2fb17d5617ef6650b4a0e9b5bf777d154
Nothemba <tnothemba@gmail.com>
2019-10-15 10:54:59 +0200

    removed display status and gave variables descriptive names

--- src/App.js
+++ src/App.js
@@ -10,90 +10,77 @@ class App extends Component {
         {
           id: 1,
           name: "artist 1",
-          display: true
         },
         {
           id: 2,
           name: "Artist 2",
-          display: true
         },
         {
           id: 3,
           name: "Artist 3",
-          display: true
         },
         {
           id: 4,
           name: "Artist 4",
-          display: true
         },
         {
           id: 5,
           name: "Artist 5",
-          display: true
         },
         {
           id: 6,
           name: "Artist 6",
-          display: true
         },
         {
           id: 7,
           name: "Artist 7",
-          display: true
         },
         {
           id: 8,
           name: "Artist 8",
-          display: true
         },
         {
           id: 9,
           name: "Artist 9",
-          display: true
+      
         },
         {
           id: 10,
           name: "Artist 10",
-          display: true
         },
         {
           id: 11,
           name: "Artist 11",
-          display: false
         },
         {
           id: 12,
           name: "Artist 12",
-          display: false
+          
         },
         {
           id: 13,
           name: "Artist 13",
-          display: false
         },
         {
           id: 14,
-          name: "Artist 14",
-          display: false
+          name: "Artist 14"
         }
       ]
     };
   }
 
   render() {
-    // const artistToRender = this.state.musicians.filter(
-    //   artist => artist.display
-    );
+    const artistToRender = this.state.musicians;
+    
     return (
       <div>
         <musician name={this.state.name} />
         <ul>
           {artistToRender
-            .map((el, i) => {
-              return <li key={el.id}>{el.name}</li>;
+            .map((element, index) => {
+              return <li key={element.id}>{element.name}</li>;
             })
-            .filter((e, k) => k < 10)}
+            .filter((element, index) => index < 10)}
         </ul>
       </div>
     );
